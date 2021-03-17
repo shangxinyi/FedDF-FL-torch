2c058d9b62d6f85db7d8ec064a32aeef6c9b06ed
shangxinyi <65947189+shangxinyi@users.noreply.github.com>
2021-03-17 21:54:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import numpy as np
-from torch import stack, div, max, eq, no_grad, tensor, save, load
+from torch import stack, div, max, eq, no_grad, save, load
 from torch.optim import SGD, Adam, lr_scheduler
 from torch.nn import CrossEntropyLoss, KLDivLoss
 from torch.nn.functional import softmax, log_softmax
@@ -8,10 +8,14 @@ from torch.utils.data.dataloader import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from light_resnet import light_resnet14
+from resnet import resnet14
+from resnet8 import ResNet
 from dataset import classify_label, make_imbalance, Indices2Dataset
 from dataset import partition_train_teach, show_clients_data_distribution
 from sampling_dirichlet import clients_indices
 from tqdm import tqdm
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 import os
 import copy
 
@@ -27,12 +31,12 @@ def args_parser():
     parser.add_argument('--num_rounds', type=int, default=100)
 
     parser.add_argument('--num_data_train', type=int, default=50000)
-    parser.add_argument('--num_data_valid', type=int, default=500)
 
     parser.add_argument('--num_epochs_local_training', type=int, default=20)
     parser.add_argument('--batch_size_local_training', type=int, default=64)
 
-    parser.add_argument('--total_steps', type=int, default=10000)
+    parser.add_argument('--total_steps', type=int, default=100)
+    parser.add_argument('--server_steps', type=int, default=100)
     parser.add_argument('--mini_batch_size', type=int, default=128)
 
     parser.add_argument('--batch_size_test', type=int, default=500)
@@ -40,12 +44,11 @@ def args_parser():
     parser.add_argument('--lr_global_teaching', type=float, default=0.001)
     parser.add_argument('--lr_local_training', type=float, default=0.1)
 
-    parser.add_argument('--temperature', type=float, default=1.)
-
+    parser.add_argument('--temperature', type=float, default=1)
     parser.add_argument('--device', type=str, default='cuda')
 
     parser.add_argument('--ratio_imbalance', type=float, default=1.)
-    parser.add_argument('--non_iid_alpha', type=float, default=1)
+   # parser.add_argument('--non_iid_alpha', type=float, default=10000000000000)
     parser.add_argument('--seed', type=int, default=123456)
     parser.add_argument('--seed1', type=int, default=987087)
     parser.add_argument('--low', type=int, default=5000)
@@ -66,69 +69,33 @@ class Global(object):
                  temperature: float,
                  device: str,
                  seed,
-                 num_online_clients: int):
+                 server_steps,
+                 num_online_clients):
         self.model = light_resnet14(num_classes)
-        self.model.to(device)
         self.model1 = light_resnet14(num_classes)
+        self.model.to(device)
         self.model1.to(device)
-        self.init_server_student = light_resnet14(num_classes)
-        self.init_server_student.to(device)
-        self.validate_model = light_resnet14(num_classes)
-        self.validate_model.to(device)
-
         self.dict_global_params = self.model.state_dict()
         self.unlabeled_data = unlabeled_data
         self.total_steps = total_steps
         self.mini_batch_size = mini_batch_size
         self.ce_loss = CrossEntropyLoss()
-        self.kld_loss = KLDivLoss(reduction="batchmean")
+        self.kld_loss = KLDivLoss(reduction='batchmean')
+        #self.optimizer = Adam(self.model.parameters(), lr=lr_global_teaching, betas=(0.9, 0.99), eps=1e-8)
         self.optimizer = Adam(self.model.parameters(), lr=lr_global_teaching)
         self.lr_scheduler = lr_scheduler.MultiStepLR(self.optimizer, milestones=[5, 20], gamma=0.1)
         self.temperature = temperature
         self.device = device
-        self.random_state = np.random.RandomState(seed)
-        self.num_online_clients = num_online_clients
         self.epoch_acc = []
         self.epoch_loss = []
+        self.random_state = np.random.RandomState(seed)
+        self.server_steps = server_steps
+        self.num_online_clients = num_online_clients
 
-    def _early_stop(self, step, list_acc):
-        idx_max = int(np.argmax(list_acc))
-        if (step+1) - ((idx_max + 1) * 100) >= 1000:
-            return True
-        else:
-            return False
-
-    def update(self, list_dicts_local_params: list, list_nums_local_data: list, data_valid):
-        # Prepare for validation data
-        images_valid = []
-        labels_valid = []
-        for datum_valid in data_valid:
-            image, label = datum_valid
-            images_valid.append(image)
-            label = tensor(label)
-            labels_valid.append(label)
-        images_valid = stack(images_valid, dim=0)
-        labels_valid = stack(labels_valid, dim=0)
-        images_valid = images_valid.to(self.device)
-        labels_valid = labels_valid.to(self.device)
-        # fedavg
+    def update(self, list_dicts_local_params: list, list_nums_local_data: list):
         self._initialize_for_model_fusion(list_dicts_local_params, list_nums_local_data)
         self.model.load_state_dict(self.dict_global_params)
-        self.init_server_student = copy.deepcopy(self.model)
-        #init_acc = self._compute_acc(images_valid, labels_valid)
-        self.init_server_student.eval()
-        with no_grad():
-            num_corrects = 0
-            outputs = self.init_server_student(images_valid)
-            _, predicts = max(outputs, -1)
-            num_corrects += sum(eq(predicts.cpu(), labels_valid.cpu())).item()
-            init_acc = num_corrects / len(images_valid)
-
-
-        # teach
         self.model.train()
-        list_params = []
-        list_acc = []
         for step in tqdm(range(self.total_steps)):
             total_indices = [i for i in range(len(self.unlabeled_data))]
             batch_indices = self.random_state.choice(total_indices, self.mini_batch_size, replace=False)
@@ -138,49 +105,27 @@ class Global(object):
                 images.append(image)
             images = stack(images, dim=0)
             images = images.to(self.device)
-
             logits_teacher = self._avg_logits(images, list_dicts_local_params)
-            # for step1 in range(self.server_steps):
-            self._teach_one_step(images, logits_teacher)
+            self._teach(images, logits_teacher)
+        self.dict_global_params = self.model.state_dict()
 
-            # validate
-            if (step + 1) % 100 == 0:
-                list_params.append(self.model.state_dict())
-                self.validate_model = copy.deepcopy(self.model)
-                self.validate_model.eval()
-                with no_grad():
-                    num_corrects = 0
-                    outputs = self.validate_model(images_valid)
-                    _, predicts = max(outputs, -1)
-                    num_corrects += sum(eq(predicts.cpu(), labels_valid.cpu())).item()
-                    acc = num_corrects / 500
-                list_acc.append(acc)
-                judge_bool = self._early_stop(step, list_acc)
-                if judge_bool:
-                    break
-        idx_max = int(np.argmax(list_acc))
-        print(f'Optimal step: {(idx_max + 1) * 100}')
-        print(f'Initial acc: {init_acc}')
-        print(f'Largest acc: {list_acc[idx_max]}')
-        if list_acc[idx_max] > init_acc:
-            print(f'FedDF win!')
-            self.dict_global_params = list_params[idx_max]
-        else:
-            print(f'FedAvg win!')
-            self.dict_global_params = self.init_server_student.state_dict()
-
-    def _teach_one_step(self, images, logits_teacher):
+    def _teach(self, images, logits_teacher):
         logits_student = self.model(images)
-        x = log_softmax(div(logits_student, self.temperature), dim=1)
-        y = softmax(div(logits_teacher, self.temperature), dim=1)
+        x = log_softmax(div(logits_student, self.temperature), -1)
+        y = softmax(div(logits_teacher, self.temperature), -1)
         loss = self.kld_loss(x, y.detach())
-        #loss = self.kld_loss(x, y)
         self.optimizer.zero_grad()
         loss.backward()
+        '''
+        for name, params in self.model.named_parameters():
+            print('--->name:', name)
+            print('--->grad_requires:', params.requires_grad)
+            print('--->grad_value:', params.grad)
+            '''
         self.optimizer.step()
 
     def _initialize_for_model_fusion(self, list_dicts_local_params: list, list_nums_local_data: list):
-        for name_param in self.dict_global_params:
+        for name_param in tqdm(self.dict_global_params):
             list_values_param = []
             for dict_local_params, num_local_data in zip(list_dicts_local_params, list_nums_local_data):
                 list_values_param.append(dict_local_params[name_param] * num_local_data)
@@ -210,7 +155,7 @@ class Global(object):
             test_loader = DataLoader(data_test, batch_size_test)
             num_corrects = 0
             list_loss = []
-            for data_batch in tqdm(test_loader):
+            for data_batch in tqdm(test_loader, desc='global testing'):
                 images, labels = data_batch
                 images, labels = images.to(self.device), labels.to(self.device)
                 outputs = self.model(images)
@@ -220,16 +165,17 @@ class Global(object):
                 list_loss.append(loss_batch.cpu().item())
             accuracy = num_corrects / len(data_test)
             loss = sum(list_loss) / len(list_loss)
+
             self.epoch_acc.append(accuracy)
             self.epoch_loss.append(loss)
 
     def download_params(self):
-        return self.dict_global_params
+        return self.model.state_dict()
 
 
 class Local(object):
     def __init__(self,
-                 dict_global_params,
+                 global_params,
                  data_client,
                  num_classes: int,
                  num_epochs_local_training: int,
@@ -237,8 +183,9 @@ class Local(object):
                  lr_local_training: float,
                  device: str):
         self.model = light_resnet14(num_classes)
+        #self.model = resnet14(num_classes)
         self.model.to(device)
-        self.model.load_state_dict(dict_global_params)
+        self.model.load_state_dict(global_params)
         self.data_client = data_client
         self.num_epochs = num_epochs_local_training
         self.batch_size = batch_size_local_training
@@ -265,30 +212,21 @@ class Local(object):
         return self.model.state_dict()
 
 
-def main():
+def feddf(non_iid_alpha):
     args = args_parser()
-    random_state = np.random.RandomState(args.seed)
-    random_state1 = np.random.RandomState(args.seed1)
+    random_state = np.random.RandomState(args.seed1)
     # Load data
     data_local_training = datasets.CIFAR10(args.path_cifar10, transform=ToTensor())
     data_global_test = datasets.CIFAR10(args.path_cifar10, train=False, transform=ToTensor())
     unlabeled_data = datasets.CIFAR100(args.path_cifar100, transform=ToTensor())
-    # Sample global validation data
-    list_label2indices_test = classify_label(data_global_test, args.num_classes)
-    indices_valid = []
-    for indices in list_label2indices_test:
-        indices_selected = random_state.choice(indices, args.num_data_valid // args.num_classes, replace=False)
-        indices_valid.extend(indices_selected)
-
-    data_valid = []
-    for idx in indices_valid:
-        data_valid.append(data_global_test[idx])
+    #unlabeled_data = data_local_training
     # Distribute data
     list_label2indices = classify_label(data_local_training, args.num_classes)
     list_label2indices_train, _ = partition_train_teach(list_label2indices, args.num_data_train, args.seed)
+    #如果需要全局不平衡，则修改low和high的值，使其不一样
     list_label2indices_imbalance = make_imbalance(list_label2indices_train, args.low, args.high, args.seed)
     list_client2indices = clients_indices(list_label2indices_imbalance, args.num_classes, args.num_clients,
-                                          args.non_iid_alpha, args.seed)
+                                          non_iid_alpha, args.seed)
     show_clients_data_distribution(data_local_training, list_client2indices, args.num_classes)
     indices2data = Indices2Dataset(data_local_training)
 
@@ -300,20 +238,22 @@ def main():
                           temperature=args.temperature,
                           device=args.device,
                           seed=args.seed,
+                          server_steps=args.server_steps,
                           num_online_clients=args.num_online_clients)
     total_clients = list(range(args.num_clients))
+
     for r in range(args.num_rounds):
         dict_global_params = global_model.download_params()
-        online_clients = random_state1.choice(total_clients, args.num_online_clients, replace=False)
+        online_clients = random_state.choice(total_clients, args.num_online_clients, replace=False)
         print(online_clients)
         list_dicts_local_params = []
         list_nums_local_data = []
         # local training
-        for client in tqdm(online_clients, desc='local_training'):
+        for client in tqdm(online_clients, desc='local training'):
             indices2data.load(list_client2indices[client])
             data_client = indices2data
             list_nums_local_data.append(len(data_client))
-            local_model = Local(dict_global_params=dict_global_params,
+            local_model = Local(global_params=dict_global_params,
                                 data_client=data_client,
                                 num_classes=args.num_classes,
                                 num_epochs_local_training=args.num_epochs_local_training,
@@ -322,22 +262,27 @@ def main():
                                 device=args.device)
             local_model.train()
             dict_local_params = local_model.upload_params()
-            list_dicts_local_params.append(dict_local_params)
+            list_dicts_local_params.append(copy.deepcopy(dict_local_params))
         # global update
         print(f'Round: [{r + 1}/{args.num_rounds}] Global Updating')
-        global_model.update(list_dicts_local_params, list_nums_local_data, data_valid)
+        global_model.update(list_dicts_local_params, list_nums_local_data)
         # global valuation
         print(f'Round: [{r + 1}/{args.num_rounds}] Global Testing')
         global_model.eval(data_global_test, args.batch_size_test)
+        print('-' * 21)
         print('Accuracy')
         print(global_model.epoch_acc)
         print('Loss')
         print(global_model.epoch_loss)
     # 保存
-    save(global_model, '\ feddf_validate_acc,epoch=20,batchsize=64,round=32.pkl')
+    save(global_model, '\ feddf_epoch=20,round=100.pkl')
     # 加载
-    # model = load('\model.pkl')
+    #model = load('\model.pkl')
+
+
 
 
 if __name__ == '__main__':
-    main()
+    args = args_parser()
+
+    feddf(1)
